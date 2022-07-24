897d452b22fae36d6b0b691fae9e36f26f4520c6
ImKal <69103102+Im-Kal@users.noreply.github.com>
2022-07-24 20:59:34 +0400

    Add files via upload

---
+++ primes.py
@@ -0,0 +1,19 @@
+primes = []
+
+def prime():
+    count = 0
+
+    for i in range(userinput1, userinput2 + 1):
+        if i > 1:
+            for j in range(2, i):
+                if i % j == 0:
+                    break
+            else:
+                primes.append(i)
+
+    print(primes)
+    
+userinput1 = int(input("Enter the 1st number = "))
+userinput2 = int(input("Enter the 2nd number = "))
+
+prime()
