1677fb0b3eaada885939cc9917fee86be3c3b585
ImKal <69103102+Im-Kal@users.noreply.github.com>
2022-08-06 20:21:41 +0400

    Add files via upload

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,3 +2,16 @@
 #   if i div 3 & 5 : print "FizzBuzz"
 #   if i div 3: print "Fizz"
 #   if i div 5: print "Buzz"
+
+for i in range(1, 101):
+    if i % 3 == 0 & i % 5 == 0:
+        print("FizzBuzz")
+    
+    elif i % 3 == 0:
+        print("Fuzz")
+    
+    elif i % 5 == 0:
+        print("Buzz")
+    
+    else: 
+        print(i)
